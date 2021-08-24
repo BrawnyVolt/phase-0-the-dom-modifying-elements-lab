b2ccb49946159f6d5b828d4a5fa40ccf0c8c63cc
Steven Brock <shyfay0@gmail.com>
2021-08-23 20:15:03 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+// document.querySelector('main').remove('#main')
+// main.remove('main')
+// document.createElement(main);
+
+$('#main').remove();
